480e07b0b5cb15c135e91b015ea9fdae7c46225c
Hyeongjin Kim <ky942400@gmail.com>
2026-06-24 17:14:28 +0900

    def update

--- src/sml_system_pkg/setup.py
+++ src/sml_system_pkg/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'sml_system_pkg'
@@ -7,9 +9,10 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    	('share/ament_index/resource_index/packages',
+        	['resource/' + package_name]),
+    	('share/' + package_name, ['package.xml']),
+    	(os.path.join('share', package_name, 'config'), glob('config/*.json')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
